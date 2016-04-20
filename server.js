f88a8199d3f1c2866334185e01f8322405fb8e6f
avecezar17 <avecezar17@mail.ru>
2016-04-21 01:17:14 +0300

    more frames video

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    fs = require('fs'),
-    ffmpeg = require('ffmpeg');
+    fs = require('fs');
 
 server.listen(3000);
 
@@ -15,26 +14,15 @@ io.sockets.on('connection', function (socket) {
         data.file = data.file.split(',')[1]; // Get rid of the data:image/png;base64 at the beginning of the file data
         var buffer = new Buffer(data.file, 'base64');
         fs.writeFile(__dirname + '/tmp/frame' + data.frame + '.png', buffer.toString('binary'), 'binary');
-        try {
-            var process = new ffmpeg('tmp/frame*.png');
-            process.then(function (video) {
-                var savedVideo = video;
-                console.log(savedVideo);
-                savedVideo./*addCommand('-r', '6').*/save('your_movie.mp4', function (error, file) {
-                    if (!error)
-                        console.log('Video file: ' + file);
-                });
-                    /*.setVideoSize('640x?', true, true, '#fff')
-                    .setAudioCodec('libfaac')*/
-                    /*.setVideoCodec('libx264')*/
-                    /*.setAudioChannels(2)*/
 
-            }, function (err) {
-                console.log('Error: ' + err);
-            });
-        } catch (e) {
-            console.log(e.code);
-            console.log(e.msg);
-        }
+        var hf = require('hls-ffmpeg');
+        var json = {
+            input: 'tmp/frame%d.png',
+            format: '848x480',
+            output: 'your_movie.mp4'
+        };
+        hf.ffmpeg(json, function(err, data){
+            console.log(err||data)
+        });
     });
 });
