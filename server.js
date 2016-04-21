b8d58a655bde378776d7dd59de2854806ed3b49b
avecezar17 <avecezar17@mail.ru>
2016-04-21 12:38:54 +0300

    html2canvas

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-var app = require('express')(),
+var express = require('express'),
+    app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
+    path = require('path'),
     fs = require('fs');
 
 server.listen(3000);
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.sockets.on('connection', function (socket) {
     socket.on('render-frame', function (data) {
